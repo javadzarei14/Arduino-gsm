aa16a12b53c6a7b8bd1d8900fe7ceafca7081d54
javadzarei14 <javadzarei1428@gmail.com>
2020-12-15 16:10:49 +0330

    comments has been deleted

---
+++ test4/test4.ino
@@ -0,0 +1,25 @@
+
+#include <Sim800l.h>
+#include <SoftwareSerial.h> //is necesary for the library!! 
+Sim800l Sim800l;  //to declare the library
+char* text;
+char* number;
+bool error; //to catch the response of sendSms
+
+
+void setup(){
+  Serial.begin(9600);
+  Sim800l.begin();// initializate the library.
+  delay(2000); 
+  text="Teste Akhar2";  //text for the message. 
+  number="09129349758";//change to a valid number.
+  Serial.println("Sending...");
+  error=Sim800l.sendSms(number,text);
+  Serial.println(error);
+  // OR 
+  //Sim800l.sendSms("+540111111111","the text go here")
+}
+
+void loop(){
+  //do nothing
+}
